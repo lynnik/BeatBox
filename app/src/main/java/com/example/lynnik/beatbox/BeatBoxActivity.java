fb344f165a9f7b19a378e3cf6fdf631d68216d28
Lynnik Vlad <lynnik.v@gmail.com>
2017-07-18 12:29:48 +0300

    Created project.

---
+++ app/src/main/java/com/example/lynnik/beatbox/BeatBoxActivity.java
@@ -0,0 +1,13 @@
+package com.example.lynnik.beatbox;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class BeatBoxActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_beat_box);
+  }
+}
